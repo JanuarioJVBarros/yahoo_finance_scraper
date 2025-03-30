f767d39e4aa1a4ffcfad588b14314cc96cbfeecc
Januario Barros <januariojvbarros@hotmail.com>
2025-03-30 14:19:19 +0200

    Added some logic to the intrinsic and current share value

--- main.py
+++ main.py
@@ -12,7 +12,14 @@ ticker_symbol = "IQ"
 ticker = yf.Ticker(ticker_symbol)
 
 # Get current share value of a stock
-yfinance_procedures.get_current_share_value(ticker)
+current_share_value = yfinance_procedures.get_current_share_value(ticker)
 
 # Calculate Stock Intrinsic value per share
-yfinance_procedures.calculate_intrinsic_value_per_share(ticker, r=DISCOUNT_RATE, g=GROWTH_RATE)
+intrincic_share_value = yfinance_procedures.calculate_intrinsic_value_per_share(ticker, r=DISCOUNT_RATE, g=GROWTH_RATE)
+
+if (intrincic_share_value - current_share_value) > 0:
+    print("undervalued")
+else:
+    print("overvalued")
+
+    
